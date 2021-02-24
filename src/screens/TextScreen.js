b297c86e650698fb250e76f5185c73890130104f
LayMui <laymui.toh@taiger.com>
2021-02-24 19:03:13 +0800

    Text input for username and password

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
     return (<View>
+        <Text>Enter Name:</Text>
         <TextInput 
             style={styles.input}
             autoCapitalize="none"
             autoCorrect={false}
+            value={name}
+            onChangeText={newValue => setName(newValue)}
         />
+        <Text>My name is {name}</Text>
+        <Text>Enter password:</Text>
+        <TextInput secureTextEntry={true} 
+        style={styles.input}
+        value={password}
+        onChangeText={newValue => setPassword(newValue)}
+        />
+        {password.length < 4 ? <Text>Password must be 4 characters</Text>: null }
         </View>
 );
 }
